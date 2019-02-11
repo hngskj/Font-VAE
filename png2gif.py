dc28214b0121388a74bfa4e065a39123ca76c219
hngskj <hngskj@gmail.com>
2019-02-11 22:57:48 +0900

    paste loc

--- png2gif.py
+++ png2gif.py
@@ -1,12 +1,12 @@
 from PIL import Image, ImageFont, ImageDraw
 import glob
 
-dir = "font_vae_cnn\plot_e100c10/"
+dir = "font_vae_cnn\plot_e20c10/"
 epoch = 20
 
 font=ImageFont.truetype("font_data/NotoSans-Regular.ttf", 30)
 
-name_list = [filename for filename in glob.glob(dir+'*.png')]
+name_list = [filename for filename in glob.glob(dir+'font_vae_mean*.png')]
 name_list = name_list[:epoch]
 img0_name = name_list[0]
 
@@ -14,7 +14,7 @@ image = Image.open(img0_name)
 epoch_draw0 = Image.new('RGBA', (200,50), "white")
 text_draw0 = ImageDraw.Draw(epoch_draw0)
 text_draw0.text((0,0), "epoch = 1/"+str(epoch), font=font, fill="black")
-image.paste(epoch_draw0, (750,120))
+image.paste(epoch_draw0, (100, 50))
 
 images_list = []
 
@@ -25,7 +25,7 @@ for idx, img in enumerate(name_list):
     text_draw = ImageDraw.Draw(epoch_draw)
     text = "epoch = "+str(idx+1)+"/"+str(epoch)
     text_draw.text((0,0), text, font=font, fill="black")
-    _img.paste(epoch_draw, (750,120))
+    _img.paste(epoch_draw, (100, 50))
     images_list.append(_img)
 
 # print(images_list)
