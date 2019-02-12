418c2f8e6fba73cfd79a2385438c4f810f595284
hngskj <hngskj@gmail.com>
2019-02-12 22:38:52 +0900

    rename

--- png2gif.py
+++ png2gif.py
@@ -1,13 +1,14 @@
 from PIL import Image, ImageFont, ImageDraw
 import glob
 
-dir = "font_vae_cnn\plot_e200c10/"
-epoch = 80
+dir = "font_vae_cnn\plot/"
+epoch = 50
+# i = 5
 
 font=ImageFont.truetype("font_data/NotoSans-Regular.ttf", 30)
 
 name_list = [filename for filename in glob.glob(dir+'font_vae_mean*.png')]
-name_list = name_list[:epoch]
+# name_list = name_list[epoch*i : epoch*(i+1)]
 img0_name = name_list[0]
 
 image = Image.open(img0_name)
@@ -24,6 +25,7 @@ for idx, img in enumerate(name_list):
     epoch_draw = Image.new('RGBA', (200,50), "white")
     text_draw = ImageDraw.Draw(epoch_draw)
     text = "epoch = "+str(idx+1)+"/"+str(epoch)
+    # text = str(idx + 1)
     text_draw.text((0,0), text, font=font, fill="black")
     _img.paste(epoch_draw, (100, 50))
     images_list.append(_img)
@@ -32,5 +34,5 @@ for idx, img in enumerate(name_list):
 image.save(dir+"_plot.gif",
            save_all=True,
            append_images=images_list[1:],
-           duration=100,
+           duration=200,
            loop=0)
